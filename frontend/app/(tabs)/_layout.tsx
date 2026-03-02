a87035f589fbbf4c5c2cf5a74ea778594f7e7b38
Derin <suerderin@outlook.com>
2026-03-01 23:58:48 -0500

    nav bar

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -5,6 +5,7 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { View } from 'react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -15,19 +16,40 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
         tabBarButton: HapticTab,
+        tabBarStyle: {
+          height: 80,
+          paddingBottom: 20,
+          paddingHorizontal: 70,
+        }
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({ color }) => (
+            <View style={{ marginTop: 5, paddingBottom: 2 }}>
+              <IconSymbol size={28} name="house.fill" color={color} />
+            </View>),
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Calendar',
+          tabBarIcon: ({ color }) => (
+            <View style={{ marginTop: 5, paddingBottom: 2 }}>
+              <IconSymbol size={28} name="calendar" color={color} />
+            </View>),
+        }}
+      />
+      <Tabs.Screen
+        name="setting"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => (
+            <View style={{ marginTop: 5, paddingBottom: 2 }}>
+              <IconSymbol size={28} name="seal" color={color} />
+            </View>),
         }}
       />
     </Tabs>
