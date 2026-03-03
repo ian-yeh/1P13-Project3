98dc0cfdec42371fc5f28115a29534f0af719388
Ian Yeh <ianyeh7@gmail.com>
2026-03-02 20:09:41 -0500

    initialized db

--- backend/src/workers/database.py
+++ backend/src/workers/database.py
@@ -1,1 +1,23 @@
 # creating database here
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from fastapi import FastAPI
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+class DatabaseWorker:
+    def __init__(self):
+        self.db = db
+
+    def write_event(self, name, date, location):
+        new_event = self.db.collection("events").document()
+        new_event.set({
+            "name": name,
+            "date": date,
+            "location": location
+        })
