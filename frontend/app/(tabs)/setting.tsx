fa205d7f135ed52342379ccaa0be7acbc3bfe9b4
Derin <suerderin@outlook.com>
2026-03-03 16:06:33 -0500

    save button

--- frontend/app/(tabs)/setting.tsx
+++ frontend/app/(tabs)/setting.tsx
@@ -1,23 +1,32 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet, View, StatusBar, TextInput } from 'react-native';
+import { useState } from 'react';
+import { React } from 'react';
 
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Fonts } from '@/constants/theme';
+import { Button } from '@react-navigation/elements';
 
 
-
+type ButtonType = {
+    children: string;
+    colorscheme: 'light' | 'dark' | "red" | "blue";
+}
 
 export default function TabTwoScreen() {
+    const [name, setName] = useState('');
+    const [passengerNumber, setPassengerNumber] = useState('');
+    const [mobilityDetails, setMobilityDetails] = useState('');
     return (
         <ParallaxScrollView
-            headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+            headerBackgroundColor={{ light: '#411919', dark: '#99c7c7' }}
             headerImage={
                 <IconSymbol
                     size={310}
-                    color="#808080"
+                    color="#adf7f0"
                     name="chevron.left.forwardslash.chevron.right"
                     style={styles.headerImage}
                 />
@@ -35,18 +44,22 @@ export default function TabTwoScreen() {
 
             <ThemedText>Full Name:</ThemedText>
             <View style={styles.container}>
-                <TextInput style={styles.inputs} />
+                <TextInput style={styles.inputs} value={name} onChangeText={setName} />
             </View>
 
             <ThemedText>Passenger number:</ThemedText>
             <View style={styles.container}>
-                <TextInput style={styles.inputs} />
+                <TextInput style={styles.inputs} value={passengerNumber} onChangeText={setPassengerNumber} />
             </View>
 
             <ThemedText>Details on Mobility devices/companions:</ThemedText>
             <View style={styles.container}>
-                <TextInput style={styles.inputs} />
+                <TextInput style={styles.inputs} value={mobilityDetails} onChangeText={setMobilityDetails} />
             </View>
+
+            <ThemedText> {name} | {passengerNumber} | {mobilityDetails}</ThemedText>
+
+            <Button colorscheme="blue" onPress={() => navigation.goBack()}>Save</Button>
 
 
         </ParallaxScrollView>
@@ -74,5 +87,6 @@ const styles = StyleSheet.create({
         margin: 12,
         borderWidth: 1,
         padding: 10,
-    }
+    },
+
 });
