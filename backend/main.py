3125a497253f05ce9ebcb83f2647ceb3475ce7d1
Omar <blaibelomar@gmail.com>
2026-03-04 18:58:41 -0500

    add caller agent

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
+from src.workers.caller import CallingAgent
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-<<<<<<< HEAD
-    return {"message": "Hello World"}
-=======
+
+
     return {"message": "Hello World"}
 
 @app.post("/schedule")
 async def schedule():
     return {"message": "Task scheduled successfully"}
->>>>>>> 3d2c596a1b7e09972fabccf436c0813f26c07862
+
--- backend/src/workers/caller.py
+++ backend/src/workers/caller.py
@@ -1,5 +1,13 @@
 # this is the code for the calling agent.
-
+from twilio.rest import Client
 class CallingAgent:
-    def __init__(self):
-        pass
+    def __init__(self,sid, auth_token, from_number):
+        self.client = Client(sid, auth_token)
+        self.from_number = from_number
+
+    def call(self, phone_number, mp3):
+        self.client.calls.create(
+            to=phone_number,
+            from_=self.from_number,
+            twiml=f'<Response><Play>{mp3}</Play></Response>')
+        
