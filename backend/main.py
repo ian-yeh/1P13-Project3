b4f4ee48b9aa9b294e1d5011aa6f9e9e7ac2bfad
Ian Yeh <ianyeh7@gmail.com>
2026-03-04 11:02:17 -0500

    fix merge conflict

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,6 @@ database_worker = DatabaseWorker()
 
 @app.get("/")
 async def root():
-<<<<<<< HEAD
-    return {"message": "Hello World"}
-=======
     return {"message": "Hello World"}
 
 @app.post("/api/schedule")
@@ -21,4 +18,3 @@ async def schedule(event_data: Event):
     )
 
     return {"message": "Task scheduled successfully"}
->>>>>>> 3d2c596a1b7e09972fabccf436c0813f26c07862
