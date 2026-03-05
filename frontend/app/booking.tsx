0bc4db6dab1b609c05f96918d437a9aedd3ad2fb
Ian Yeh <ianyeh7@gmail.com>
2026-03-05 15:14:06 -0500

    change booking state

--- frontend/app/booking.tsx
+++ frontend/app/booking.tsx
@@ -3,7 +3,7 @@ import { View, Text, TextInput, TouchableOpacity } from "react-native"
 import { scheduleEvent } from "@/api/api";
 import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
-//import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 
 const BookingPage = () => {
@@ -11,14 +11,14 @@ const BookingPage = () => {
   const userData: any = useUser.getState();
 
   const [location, setLocation] = useState('');
-  const [arrivalTime, setArrivalTime] = useState<Date | null>(null)
-  const [departureTime, setDepartureTime] = useState(null)
+  const [arrivalTime, setArrivalTime] = useState<Date>(new Date())
+  const [departureTime, setDepartureTime] = useState<Date>(new Date())
 
   const handleBook = async () => {
       const response = await scheduleEvent({
         name: "Mark",
         date: "2026-03-05T19:15:11.660Z",
-        location: "Design Studio",
+        location: location,
         arrival_time: arrivalTime || "2026-03-05T19:15:11.660Z",
         departure_time: departureTime || "2026-03-05T19:15:11.660Z",
         user_id: "1"
@@ -44,19 +44,23 @@ const BookingPage = () => {
 
       <View>
         <Text className="mt-4">Departure Time</Text>
-        <TextInput 
-          value={location} 
-          //onChangeText={setDepartureTime} 
-          className="border-2 border-gray-200 p-2 mt-1 rounded-lg"
+        <DateTimePicker 
+          value={departureTime}
+          mode="date"
+          onChange={(event, date) => {
+            if (date) setDepartureTime(date)
+          }}
         />
       </View>
 
       <View>
         <Text className="mt-4">Arrival Time</Text>
-        <TextInput 
-          value={location} 
-          //onChangeText={setArrivalTime} 
-          className="border-2 border-gray-200 p-2 mt-1 rounded-lg"
+        <DateTimePicker 
+          value={arrivalTime}
+          mode="date"
+          onChange={(event, date) => {
+            if (date) setArrivalTime(date)
+          }}
         />
       </View>
 
