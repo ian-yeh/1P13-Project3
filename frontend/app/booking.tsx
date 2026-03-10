141430f79b670196e88479dcc2c7deddea67b6c8
Ian Yeh <ianyeh7@gmail.com>
2026-03-10 15:51:32 -0400

    improvements to UI; removed themed texts

--- frontend/app/booking.tsx
+++ frontend/app/booking.tsx
@@ -2,72 +2,93 @@ import { useUser } from "@/store/useStore"
 import { View, Text, TextInput, TouchableOpacity } from "react-native"
 import { scheduleEvent } from "@/api/api";
 import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
-import DateTimePicker from '@react-native-community/datetimepicker';
+import { useState, useRef } from "react";
+import { DatePicker } from "@s77rt/react-native-date-picker";
+import type { DatePickerHandle } from "@s77rt/react-native-date-picker";
 
 
 const BookingPage = () => {
+  const departurePickerRef = useRef<DatePickerHandle>(null);
+  const arrivalPickerRef = useRef<DatePickerHandle>(null);
   const navigator = useNavigation();
   const userData: any = useUser.getState();
 
   const [location, setLocation] = useState('');
-  const [arrivalTime, setArrivalTime] = useState<Date>(new Date())
-  const [departureTime, setDepartureTime] = useState<Date>(new Date())
+  const [arrivalTime, setArrivalTime] = useState<Date | null>(null)
+  const [departureTime, setDepartureTime] = useState<Date | null>(null)
 
   const handleBook = async () => {
-      const response = await scheduleEvent({
-        name: "Mark",
-        date: "2026-03-05T19:15:11.660Z",
-        location: location,
-        arrival_time: arrivalTime || "2026-03-05T19:15:11.660Z",
-        departure_time: departureTime || "2026-03-05T19:15:11.660Z",
-        user_id: "1",
-        status: "pending"
-      })
+    const response = await scheduleEvent({
+      name: "Mark",
+      date: new Date(),
+      location: location,
+      arrival_time: arrivalTime,
+      departure_time: departureTime,
+      user_id: "1",
+      status: "pending"
+    })
 
-      console.log(response)
+    console.log(response)
 
-      navigator.goBack();
+    navigator.goBack();
   }
 
   return (
-    <View className="bg-white h-full px-4 py-8">
-      <Text className="text-black font-bold text-3xl">Book your ride, {userData.name}.</Text>
+    <View className="bg-white h-full px-4 py-8 flex-1 items-center">
+      <View className="max-w-[400px]">
+        <Text className="text-black font-bold text-3xl">Book your ride, {userData.name}.</Text>
 
-      <View>
-        <Text className="mt-4">Where do you want to go?</Text>
-        <TextInput 
-          value={location} 
-          onChangeText={setLocation} 
-          className="border-2 border-gray-200 p-2 mt-1 rounded-lg"
-        />
+        <View>
+          <Text className="mt-4 text-lg font-semibold">Enter Destination</Text>
+          <TextInput
+            value={location}
+            onChangeText={setLocation}
+            className="border-2 border-gray-200 p-2 mt-1 rounded-lg"
+          />
+        </View>
+
+        <View>
+          <Text className="mt-4 font-semibold text-lg">Departure Time</Text>
+
+          <TouchableOpacity
+            className="border-2 border-gray-200 p-2 mt-1 rounded-lg"
+            onPress={() => departurePickerRef.current?.showPicker()}
+          >
+            <Text>{departureTime ? departureTime.toString() : "Select date"}</Text>
+          </TouchableOpacity>
+
+          <DatePicker
+            ref={departurePickerRef}
+            type="datetime"
+            value={departureTime}
+            onChange={setDepartureTime}
+          />
+        </View>
+
+        <View>
+          <Text className="mt-4 font-semibold text-lg">Arrival Time</Text>
+
+          <TouchableOpacity
+            className="border-2 border-gray-200 p-2 mt-1 rounded-lg"
+            onPress={() => arrivalPickerRef.current?.showPicker()}
+          >
+            <Text>{arrivalTime ? arrivalTime.toString() : "Select date"}</Text>
+          </TouchableOpacity>
+
+          <DatePicker
+            ref={arrivalPickerRef}
+            type="datetime"
+            value={arrivalTime}
+            onChange={setArrivalTime}
+          />
+        </View>
+
+        <View className="flex-1 items-center">
+          <TouchableOpacity className="flex items-center p-2 bg-blue-200 w-36 mt-8 rounded-lg" onPress={handleBook}>
+            <Text>Book ride</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-
-      <View>
-        <Text className="mt-4">Departure Time</Text>
-        <DateTimePicker 
-          value={departureTime}
-          mode="date"
-          onChange={(event, date) => {
-            if (date) setDepartureTime(date)
-          }}
-        />
-      </View>
-
-      <View>
-        <Text className="mt-4">Arrival Time</Text>
-        <DateTimePicker 
-          value={arrivalTime}
-          mode="date"
-          onChange={(event, date) => {
-            if (date) setArrivalTime(date)
-          }}
-        />
-      </View>
-
-      <TouchableOpacity className="flex items-center p-2 bg-blue-200 w-36 mt-8 rounded-lg" onPress={handleBook}>
-        <Text>Book ride</Text>
-      </TouchableOpacity>
     </View>
   )
 }
